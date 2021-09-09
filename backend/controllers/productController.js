eac4463fecf8c0be1bc0257a1899998edfbba416
sudeepac <sudeepac@gmail.com>
2021-09-10 01:07:52 +1000

    Search, Pagination, Product Carousel implemented

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,8 +5,22 @@ import Product from "../models/productModel.js";
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
-  res.json(products);
+  const pageSize = 10;
+  const page = Number(req.query.pageNumber) || 1;
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: "i",
+        },
+      }
+    : {};
+
+  const count = await Product.countDocuments({ ...keyword });
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc Fetch single products
@@ -125,6 +139,15 @@ const createProductReview = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Get top rated products
+// @route   GET /api/products/top
+// @access  Public
+const getTopProducts = asyncHandler(async (req, res) => {
+  const products = await Product.find({}).sort({ rating: -1 }).limit(3);
+
+  res.json(products);
+});
+
 export {
   getProducts,
   getProductById,
@@ -132,4 +155,5 @@ export {
   createProduct,
   updateProduct,
   createProductReview,
+  getTopProducts,
 };
---
+++ frontend/src/components/SearchBox.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Form, Button, Row, Col } from "react-bootstrap";
+
+const SearchBox = ({ history }) => {
+  const [keyword, setKeyword] = useState("");
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if (keyword.trim()) {
+      history.push(`/search/${keyword}`);
+    } else {
+      history.push("/");
+    }
+  };
+
+  return (
+    <Form onSubmit={submitHandler} inline>
+      <Row>
+        <Col>
+          <Form.Control
+            type="text"
+            name="q"
+            onChange={(e) => setKeyword(e.target.value)}
+            placeholder="Search Products..."
+            className="mr-sm-2 ml-sm-5"
+            style={{ width: "250px" }}
+          ></Form.Control>
+        </Col>
+        <Col>
+          <Button type="submit" variant="outline-success" className="p-2">
+            Search
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default SearchBox;
